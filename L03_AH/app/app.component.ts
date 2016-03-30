514b0567c95b3e1f15a0e1312665df07f915135f
Marius Cristea <marius.cristea@irian.ro>
2016-03-30 15:23:45 +0200

    NG - Hello World - final

--- L03_AH/app/app.component.ts
+++ L03_AH/app/app.component.ts
@@ -1,5 +1,11 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'my-app',
+    template: ''
+})
 export class AppComponent {
     constructor() {
-        console.log('Hello from TS!!!!');
+        console.log('Hello from Angular 2!!!!');
     }
 }
