294da425c585b70910850370e4aa0190359625ef
Marius Cristea <marius.cristea@irian.ro>
2016-04-04 17:32:39 +0300

    L04 - NG - Pipes - setup

--- L04_AP/app/data.ts
+++ L04_AP/app/data.ts
@@ -5,14 +5,16 @@ export var PIZZAS: Pizza[] = [
     name: '4 STAGIONI',
     price: 27.5,
     weight: 550,
+    image: 'images/quattro.png',
     ingredients: 'sos rosii, mozzarella, ciuperci, salam, sunca presata, oregano, anghinare',
     canPurchase: true,
     soldOut: false
   },
   {
-    name: 'DIAVOLA',
+    name: 'PEPPERONI',
     price: 25,
     weight: 450,
+    image: 'images/pepperoni.png',
     ingredients: 'sos rosii, mozzarella, salam picant, oregano',
     canPurchase: false,
     soldOut: false
