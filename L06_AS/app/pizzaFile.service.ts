93972f9ee39cf38e8c06d4893c7e6ed74df2633e
Marius Cristea <marius.cristea@irian.ro>
2016-05-04 14:15:28 +0300

    L06 - NG - Refactor - init

--- L06_AS/app/pizzaFile.service.ts
+++ L06_AS/app/pizzaFile.service.ts
@@ -1,11 +1,12 @@
 import {Injectable} from 'angular2/core';
 
+import {PizzaService} from './pizza.service';
 import {Pizza} from './pizza';
 import {PIZZAS} from './data';
 
 @Injectable()
-export class PizzaFileService{
-  getPizzas():Pizza[]{
-    return PIZZAS;
+export class PizzaFileService implements PizzaService {
+  getPizzas():Promise<Pizza[]>{
+    return Promise.resolve(PIZZAS);
   }
 }
